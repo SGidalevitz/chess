3789743d61a87265cd709c1daf06057b2bd42e88
sgidalevitz <simon.gidalevitz@gmail.com>
2024-03-28 20:16:43 -0500

    Made the tests for invalid FEN iterate over a String[] instead of repeating the same assertThrows each time

--- src/BoardTest.java
+++ src/BoardTest.java
@@ -32,34 +32,39 @@ public class BoardTest {
     }
     @Test
     public void testInvalidFENs() {
-        // Test first FEN argument - position
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbknr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"); });
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/9/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"); });
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/4p4/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"); });
+        String[] FENsToTest = {
+                // Test first FEN argument - position
+                "rnbqkbknr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+                "rnbqkbnr/pppppppp/8/8/9/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+                "rnbqkbnr/pppppppp/8/8/4p4/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
 
-        // Test second FEN argument - whose move it is
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR k KQkq - 0 1"); });
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR hello KQkq - 0 1"); });
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR 136 KQkq - 0 1"); });
+                // Test second FEN argument - whose move it is
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR k KQkq - 0 1",
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR hello KQkq - 0 1",
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR 136 KQkq - 0 1",
 
-        // Test third FEN argument - castling rights
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w kqKQ - 0 1"); });
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w kK - 0 1"); });
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w awesome - 0 1"); });
+                // Test third FEN argument - castling rights
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w kqKQ - 0 1",
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w kK - 0 1",
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w awesome - 0 1",
 
-        // Test fourth FEN argument - en passant target square
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq hello 0 1"); });
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq z5 0 1"); });
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq q11 0 1"); });
+                // Test fourth FEN argument - en passant target square
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq hello 0 1",
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq z5 0 1",
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq q11 0 1",
 
-        // Test fifth FEN argument - half move clock
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 120 1"); });
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - -20 1"); });
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 2390984 1"); });
+                // Test fifth FEN argument - half move clock
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 120 1",
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - -20 1",
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 2390984 1",
 
-        // Test sixth FEN argument - full move number
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0"); });
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 -120"); });
-        assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 -39489"); });
+                // Test sixth FEN argument - full move number
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0",
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 -120",
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 -39489"
+        };
+        for (String FEN : FENsToTest) {
+            assertThrows(IllegalArgumentException.class, () -> { Board testBoard = new Board(FEN); });
+        }
     }
 }
