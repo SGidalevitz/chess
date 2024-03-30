dcf846caafb22bf9ace5e997dda49fc3158a11fa
sgidalevitz <simon.gidalevitz@gmail.com>
2024-03-29 21:17:44 -0500

    Fixed BoardTest for testing FEN generation and a test that unexpectedly got through in testResultsInCheck().

--- src/BoardTest.java
+++ src/BoardTest.java
@@ -76,23 +76,16 @@ public class BoardTest {
         // Test position 1
         Board board = new Board("8/3k3p/1p6/1Np5/2P3P1/4p3/3bK3/8 w - - 0 54");
         board.makeMove("g4", "g5");
-        board.switchToMove();
         assertEquals("8/3k3p/1p6/1Np3P1/2P5/4p3/3bK3/8 b - - 0 54", board.getFEN());
 
         //Test position 2
         board = new Board("r1b1qrkb/pp1n1p1p/4p1pP/2pnP1B1/3p2N1/3P1NP1/PPP2PB1/R2QR1K1 b - - 6 16");
         board.makeMove("d7", "b6");
-        board.switchToMove();
-        board.incrementFullMoveNumber();
-        board.incrementHalfMoveClock();
         assertEquals("r1b1qrkb/pp3p1p/1n2p1pP/2pnP1B1/3p2N1/3P1NP1/PPP2PB1/R2QR1K1 w - - 7 17", board.getFEN());
 
         //Test position 3
         board = new Board("3N1R2/P7/5k2/5bp1/r7/r4BK1/6P1/8 b - - 10 53");
         board.makeMove("f6", "e5");
-        board.switchToMove();
-        board.incrementHalfMoveClock();
-        board.incrementFullMoveNumber();
         assertEquals("3N1R2/P7/8/4kbp1/r7/r4BK1/6P1/8 w - - 11 54", board.getFEN());
 
     }
@@ -176,7 +169,7 @@ public class BoardTest {
         board = new Board("r1b3nr/1ppp1p2/2n1pkp1/p5pP/1NBNP3/1P4q1/P1PP1PP1/R1B1K2R w KQ - 0 15");
         assertTrue(board.resultsInCheck("f2", "f3", PieceColor.White));
         assertTrue(board.resultsInCheck("f2", "f4", PieceColor.White));
-        assertFalse(board.resultsInCheck("c3", "c3", PieceColor.White));
+        assertFalse(board.resultsInCheck("c2", "c3", PieceColor.White));
     }
     public static boolean equalsIgnoringOrder(ArrayList<Position> list0, ArrayList<Position> list1) {
         return list0.containsAll(list1) && list0.size() == list1.size();
